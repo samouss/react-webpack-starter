e2854546c4d79bd0ce9b6826c71dfd770b7f6333
Samuel Vaillant <samuel.vllnt@gmail.com>
2016-09-15 16:32:36 +0200

    fix: remove useless babel configuration, use babelrc

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -24,15 +24,6 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         loader: 'babel',
-        query: {
-          presets: [
-            'es2015',
-            'react',
-          ],
-          plugins: [
-            'transform-object-rest-spread',
-          ],
-        },
       },
       {
         test: /\.html$/,
