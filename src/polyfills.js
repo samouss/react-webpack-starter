1d06b0987c1d38805d47235ad54908f12a52a11f
Vaillant Samuel <samuel.vllnt@gmail.com>
2017-08-24 16:47:51 +0200

    core: use preset env instead of preset-XXXX

--- src/polyfills.js
+++ src/polyfills.js
@@ -1,8 +1,4 @@
-// Genric polyfill ex:
+// Generic polyfill ex:
 // import 'whatwg-fetch';
 
-// Import core-js modules ex:
-// import 'core-js/es6/promise';
-
-// Import core-js functions ex:
-// import 'core-js/fn/array/includes';
+import 'babel-polyfill';
